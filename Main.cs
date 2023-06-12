e83a6ce2a2e56fca5e849e7ab59f75fe469ecc5d
EdenFails <63517645+EdenFails@users.noreply.github.com>
2023-06-12 16:00:58 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿
+using MelonLoader;
+using System;
+using Patches;
+using System.Reflection;
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("QMFreezeVrchat")]
+[assembly: AssemblyProduct("QMFreeze")]
+[assembly: MelonInfo(typeof(Helix.Main), "QMFreeze-ReMade", "OwO", "EdenFails", null)]
+[assembly: MelonGame("VRChat", null)]
+[assembly: MelonColor(ConsoleColor.DarkRed)]
+//[assembly: TargetFramework(".NETFramework,Version=v4.7.2", FrameworkDisplayName = ".NET Framework 4.7.2")]
+
+
+
+
+namespace Helix
+{
+
+    public class Main : MelonMod
+    {
+        public Main() 
+        {
+
+
+        }
+
+        public override void OnApplicationStart()
+        {
+            PatchBS.Patch();
+            PatchBS.QMFreeze = true;
+        }
+
+      
+
+    } 
+}
---
+++ Patches.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using MelonLoader;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Patches
+{
+
+    internal static class PatchBS
+    {
+
+
+        public static List<GameObject> ClonedAvatar = new List<GameObject>();
+        private static readonly HarmonyLib.Harmony instance = new HarmonyLib.Harmony("QMFreeze");
+
+        public static unsafe void Patch()
+        {
+            instance.Patch(typeof(VRC.UI.Elements.QuickMenu).GetMethod(nameof(VRC.UI.Elements.QuickMenu.OnDisable)), new HarmonyLib.HarmonyMethod(typeof(PatchBS).GetMethod(nameof(OnMenuClosed), BindingFlags.Static | BindingFlags.NonPublic)), null, null, null, null);
+
+            instance.Patch(typeof(VRC.UI.Elements.QuickMenu).GetMethod(nameof(VRC.UI.Elements.QuickMenu.OnEnable)), new HarmonyLib.HarmonyMethod(typeof(PatchBS).GetMethod(nameof(OnMenuOpened), BindingFlags.Static | BindingFlags.NonPublic)), null, null, null, null);
+
+            MelonLogger.Msg("QMFreeze Patched");
+        }
+
+        public static Vector3 _origionalGravity, _originalVelocity;
+        public static bool isfrozen, QMFreeze = false;
+        private static void OnMenuOpened()
+        {
+
+            if (QMFreeze)
+            {
+                VRCPlayer.field_Internal_Static_VRCPlayer_0.prop_VRCPlayerApi_0.Immobilize(true);
+                _origionalGravity = Physics.gravity;
+                _originalVelocity = Networking.LocalPlayer.GetVelocity();
+                Physics.gravity = Vector3.zero;
+                Networking.LocalPlayer.SetVelocity(Vector3.zero);
+                isfrozen = true;
+            }
+        }
+        private static void OnMenuClosed()
+        {
+
+            if (isfrozen)
+            {
+                Physics.gravity = _origionalGravity;
+                VRCPlayer.field_Internal_Static_VRCPlayer_0.prop_VRCPlayerApi_0.Immobilize(false);
+                //Networking.LocalPlayer.SetVelocity(_originalVelocity);
+                isfrozen = false;
+            }
+        }
+
+    }
+    
+}
+
